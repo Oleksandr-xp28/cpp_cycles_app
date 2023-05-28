68e682cdf41eb951913b8ed88351c7b8cb58d1a9
Oleksandr-xp28 <fedorut20@gmail.com>
2023-05-28 11:19:49 +0300

    done

--- main_cycles.cpp
+++ main_cycles.cpp
@@ -1,12 +1,34 @@
 // cpp_cycles_app.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// feature/hm_2/ex_1
 
 #include <iostream>
 using namespace std;
- 
+
+bool TwoIdenDig(int num) {
+    int digit1 = num % 10;
+    int digit2 = (num / 10) % 10;
+    int digit3 = num / 100;
+
+    if (digit1 == digit2 || digit1 == digit3 || digit2 == digit3) {
+        return true;
+    }
+
+    return false;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+     int count = 0;
+
+    for (int num = 100; num <= 999; num++) {
+        if (TwoIdenDig(num)) {
+                count++;
+        }
+    }
+
+    cout << "Number of integers with two identical digits: " << count << endl;
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu

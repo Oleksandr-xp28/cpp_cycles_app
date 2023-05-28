e586595232f5e9e5b55182095ece345ce879a972
Oleksandr-xp28 <fedorut20@gmail.com>
2023-05-28 10:34:49 +0300

    Resolved merge conflict by incorporating both suggestions.

--- main_cycles.cpp
+++ main_cycles.cpp
@@ -6,19 +6,7 @@ using namespace std;
 
 int main()
 {
-	int a;
-	int sum = 0;
 
-	std::cout << "Enter a number (a): ";
-	std::cin >> a;
-
-	for (int i = a; i <= 500; i++) {
-		sum += i;
-	}
-
-	cout << "Sum of numbers from " << a << " to 500: " << sum << endl;
-
-	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu

a6c1371848534738075b8d909fca3269ddf76b40
Oleksandr-xp28 <fedorut20@gmail.com>
2023-05-28 11:05:46 +0300

    task 5 done

--- main_cycles.cpp
+++ main_cycles.cpp
@@ -6,7 +6,19 @@ using namespace std;
 
 int main()
 {
+	int option;
 
+	cout << "Enter an option: ";
+	cin >> option;
+
+	cout << "Multiplication table for " << option << ":" << endl;
+
+	for (int i = 2; i <= 10; i++) {
+		int result = option * i;
+		cout << option << " x " << i << " = " << result << endl;
+	}
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
